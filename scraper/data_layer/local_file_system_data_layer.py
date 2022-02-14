32d607797adf6b72ee4dcda4cb88cfd2faa3c8d5
kulspel <bjoern.pe@gmail.com>
2022-02-14 22:42:41 +0100

    test change

--- scraper/data_layer/local_file_system_data_layer.py
+++ scraper/data_layer/local_file_system_data_layer.py
@@ -4,4 +4,5 @@ from scraper.data_layer.data_layer import DataLayer
 class LocalFileSystem(DataLayer):
 
     def save(self, data_layer_identifier: str, data):
+        path = data_layer_identifier.split("::")
         return super().save(data_layer_identifier, data)
