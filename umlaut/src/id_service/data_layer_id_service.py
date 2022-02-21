ee3465d9baa0441b8017186aafd807b9309e603d
kulspel <bjoern.pe@gmail.com>
2022-02-21 22:41:19 +0100

    Found the bug with the duplicated ids

--- umlaut/src/id_service/data_layer_id_service.py
+++ umlaut/src/id_service/data_layer_id_service.py
@@ -17,14 +17,15 @@ class DataLayerIdService(IdService):
         metadata = self.data_layer.load_metadata(class_identifier)
 
         if metadata and 'id_service' in metadata:
-            id_counter = metadata['id_service']['id_counter']
+            id_counter = metadata['id_service']['id_counter'] + 1
             self.data_layer.update_metadata(
                 class_identifier,
-                {"id_service": {"id_counter": id_counter+1}})
+                {"id_service": {"id_counter": id_counter}})
 
             return id_counter
         else:
             # NOTE This might be redundant, with the defaults of Metadata.py
+            # NOTE there no longer are any defaults in Metadata.py,
             id_counter = 1
             self.data_layer.update_metadata(
                 class_identifier,
